c898623e00c77e6b63d059702acd94957d5ba985
Vadalan <naresh.vadala@cdk.com>
2016-09-21 17:51:06 +0530

    Added product list in Product constructor chamges in inventory app

--- app/inventoryApp.component.ts
+++ app/inventoryApp.component.ts
@@ -5,16 +5,35 @@ import { Product } from './product';
 @Component({
     selector: 'inventory-app',
     directives: [ProductList],
-    template : `<div class = "inventory-app">
+    template: `<div class = "inventory-app">
                     <products-list [productList]="products" (onProductSelected)="productWasSelected($event)">
                     </products-list>
                 </div>
                 `
 })
-export class InventoryApp{
-    products : Product[];
-    constructor()
-    {
-        this.products = [new Product()]
+export class InventoryApp {
+    products: Product[];
+    constructor() {
+        this.products = [
+            new Product(
+                'MYSHOES', 'Black  Running  Shoes',
+                '/resources/images/products/black-shoes.jpg',
+                ['Men', 'Shoes', 'Running  Shoes'],
+                09.99),
+            new Product(
+                'NICEHAT', 'A  Nice  Black  Hat',
+                '/resources/images/products/black-hat.jpg',
+                ['Men', 'Accessories', 'Hats'],
+                29.99),
+            new Product(
+                'NEATOJACKET', 'Blue  Jacket',
+                '/resources/images/products/blue-jacket.jpg',
+                ['Women', 'Apparel', 'Jackets  &  Vests'],
+                238.9)
+        ];
+    }
+
+    productWasSelected(product: Product): void {
+        console.log('Product  clicked:  ', product);
     }
 }
--- app/product.ts
+++ app/product.ts
@@ -1,12 +1,10 @@
-class Product
-{
+export class Product {
     public sku: string;
     public name: string;
     public imageUrl: string;
-    public department : string[];
-    public price : number;
-    constructor()
-    {
-        
+    public department: string[];
+    public price: number;
+    constructor(sku: string, name: string, imageUrl: string, department: string[], price: number) {
+
     }
 }
